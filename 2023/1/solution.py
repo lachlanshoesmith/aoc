7c65210ebba02630d7d39336ac74cc6425511f13
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2023-12-12 22:14:11 +1100

    👍 complete day 1 pt 2

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -8,8 +8,13 @@ if len(sys.argv) != 2:
 with open(sys.argv[1]) as f:
     lines = f.readlines()
     sum = 0
+    strings = [['nine', 'n9e'], ['eight', 'e8t'], ['seven', 's7n'], ['six', 's6x'], [
+        'five', 'f5e'], ['four', 'f4r'], ['three', 't3e'], ['two', 't2o'], ['one', 'o1e']]
     for line in lines:
         nums = []
+        for i, num in enumerate(strings):
+            if num[0] in line:
+                line = line.replace(strings[i][0], strings[i][1])
         for char in line:
             if char.isdigit():
                 nums.append(char)
